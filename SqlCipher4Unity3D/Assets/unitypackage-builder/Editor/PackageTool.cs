2b44302419492d6a5b2d5e9908a23cae340c73bb
netpyoung <netpyoung@gmail.com>
2020-03-09 00:54:14 +0900

    cleanup

--- SqlCipher4Unity3D/Assets/unitypackage-builder/Editor/PackageTool.cs
+++ SqlCipher4Unity3D/Assets/unitypackage-builder/Editor/PackageTool.cs
@@ -5,7 +5,7 @@ public class PackageTool
     [MenuItem("Package/Update Package")]
     private static void UpdatePackage()
     {
-        const string version = "v1.0.1";
+        const string version = "v1.0.2";
         AssetDatabase.ExportPackage(new string[] { "Assets/SqlCipher4Unity3D" }, $"../SqlCipher4Unity3D-{version}.unitypackage", ExportPackageOptions.Recurse);
     }
 }
